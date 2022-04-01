8b80c1fa21968a7c584b5bb498590607ccb9b581
arnabm14 <arnavm014@gmail.com>
2022-04-01 17:41:59 +0530

    Fixed the code and Procfile

--- app.py
+++ app.py
@@ -1,12 +1,53 @@
-from flask import Flask, render_template
-from flask import json 
-from flask import jsonify, make_response
+from flask import Flask, render_template, url_for, request, redirect
+# import sqlalchemy
+from datetime import datetime
+from flask import jsonify
+from flask import json
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# db = sqlalchemy(app)
 
-@app.route('/')
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.String(200), nullable=False)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+#     def __repr__(self):
+#         return '<Task %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    print("Working")
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update.html', task=task)
 
 
 @app.route('/banners')
@@ -41,5 +82,6 @@ def categories():
 def products():
     return jsonify(json.load(open("products.json")))
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
