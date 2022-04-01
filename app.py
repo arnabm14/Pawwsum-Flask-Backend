59656281d15dafcb09af8ad680c67d953276d68d
arnabm14 <arnavm014@gmail.com>
2022-04-01 19:06:48 +0530

    Heroku Python 3.9.11 build with json

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask import jsonify
+from flask import json
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
