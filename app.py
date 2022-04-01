840f3615cbdf78bcd1c081ecfb4cfea586117e0e
arnabm14 <arnavm014@gmail.com>
2022-04-01 17:19:37 +0530

    Added Procfile and other details

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import json 
 from flask import jsonify, make_response
 
@@ -6,10 +6,12 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
+
 
 @app.route('/banners')
 def banner():
+    # return render_template('banners.json')
     data=open("banners.json")
     dataj=json.load(data)
     response = app.response_class(
@@ -18,13 +20,14 @@ def banner():
         mimetype='application/json'
     )
     return response
-    # print(type(json.dumps(dataj)))
+    print(type(json.dumps(dataj)))
           
-    # return json.dumps(dataj)
+    return json.dumps(dataj)
 
 
 @app.route('/categories')
 def categories():
+    # return render_template('categories.json')
     data=open("categories.json")
     dataj=json.load(data)
     response = app.response_class(
