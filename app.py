66607a40a32b3c6630a3c5bbdc15e1e8875c5013
arnabm14 <arnavm014@gmail.com>
2022-04-01 19:04:37 +0530

    Heroku Python 3.9.11 build

--- app.py
+++ app.py
@@ -60,6 +60,39 @@ def update(id):
     else:
         return render_template('update.html', task=task)
 
+@app.route('/banners')
+def banner():
+    # return render_template('banners.json')
+    data=open("banners.json")
+    dataj=json.load(data)
+    response = app.response_class(
+        response=json.dumps(dataj),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+    print(type(json.dumps(dataj)))
+          
+    return json.dumps(dataj)
+
+
+@app.route('/categories')
+def categories():
+    # return render_template('categories.json')
+    data=open("categories.json")
+    dataj=json.load(data)
+    response = app.response_class(
+        response=json.dumps(dataj),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route('/products')
+def products():
+    return jsonify(json.load(open("products.json")))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
