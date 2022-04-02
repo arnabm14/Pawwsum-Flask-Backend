65897dcef59bd26ac53ab598dd68f9ee33f5b448
Arnab Mondal <arnavm014@gmail.com>
2022-04-02 12:40:10 +0530

    Commented our SQLAlchemy

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask import jsonify
 from flask import json
